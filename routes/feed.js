63a6cedd7d3301fe82a81128c027227a8b170aef
wkodate <n9lfcth9@gmail.com>
2014-09-08 14:37:22 +0000

    update feedparser #1

--- routes/feed.js
+++ routes/feed.js
@@ -1,30 +1,36 @@
+// feedparser - https://www.npmjs.org/package/feedparser
 var FeedParser = require('feedparser')
-  , request = require('request');
+    , request = require('request');
 
-var req = request('http://kizasi.jp/rss.xml')
-  , feedparser = new FeedParser();
+var req = request('http://b.hatena.ne.jp/entrylist?sort=hot&mode=rss')
+    , feedparser = new FeedParser(); // new FeedParser([options])でoptions設定
 
 req.on('error', function (error) {
-  // handle any request errors
+    // リクエストエラー処理
 });
 req.on('response', function (res) {
-  var stream = this;
-
-  if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
-
-  stream.pipe(feedparser);
+    var stream = this;
+    if (res.statusCode != 200) {
+        return this.emit('error', new Error('Bad status code'));
+    }
+    stream.pipe(feedparser);
 });
 
 feedparser.on('error', function(error) {
-  // always handle errors
+    // 通常のエラー処理
 });
 feedparser.on('readable', function() {
-  // This is where the action is!
-  var stream = this
-    , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-    , item;
-
-  while (item = stream.read()) {
-    console.log(item);
-  }
+    // 処理ロジックを書く
+    // metaプロパティはfeedeparserインスタンスのコンテキストに常に置き換える
+    var stream = this
+        , meta = this.meta
+        , item;
+    while (item = stream.read()) {
+        console.log(
+            item.title + "\t" 
+            + item.description + "\t" 
+            + item.link + "\t" 
+            + item.date
+            );
+    }
 });
