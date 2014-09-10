f37a884bb4610327176d61131ae6c7218f1fbabc
wkodate <n9lfcth9@gmail.com>
2014-09-10 16:26:02 +0000

    add connection to mongodb #2

--- routes/feed.js
+++ routes/feed.js
@@ -2,7 +2,24 @@
 var FeedParser = require('feedparser')
     , request = require('request');
 
-var req = request('http://b.hatena.ne.jp/entrylist?sort=hot&mode=rss')
+// Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testdb', function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+var buzzwordSchema = mongoose.Schema({
+    name: String,
+    date: Date,
+    xmlurl: String
+});
+var Buzzword = mongoose.model('Buzzword', buzzwordSchema);
+
+// RSS URL
+var xmlurl = 'http://kizasi.jp/rss.xml';
+
+var req = request(xmlurl)
     , feedparser = new FeedParser(); // new FeedParser([options])でoptions設定
 
 req.on('error', function (error) {
@@ -25,12 +42,17 @@ feedparser.on('readable', function() {
     var stream = this
         , meta = this.meta
         , item;
+
+    // 取得したアイテムをMongoDBに保存
+    var buzzword = new Buzzword();
     while (item = stream.read()) {
-        console.log(
-            item.title + "\t" 
-            + item.description + "\t" 
-            + item.link + "\t" 
-            + item.date
-            );
+        buzzword.name = item.title;
+        buzzword.date = item.date;
+        buzzword.xmlurl = xmlurl;
+        buzzword.save(function(err, buzzword) {
+            if (err) {
+                console.log(err);
+            }
+        });
     }
-});
+})
